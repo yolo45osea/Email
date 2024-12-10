44ea9455879ad208279575212791ef773ce6018f
benjamin romero <133557177+yolo45osea@users.noreply.github.com>
2024-12-10 02:08:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
 
