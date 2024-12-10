bacc5376df76992516881e3ac3745f52cb3c9b30
benjamin romero <133557177+yolo45osea@users.noreply.github.com>
2024-12-09 23:30:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const app = express();
+
+// Configurar CORS para permitir todo
+app.use(cors());
+app.use(express.json());
+
+app.post('/send-email', async (req, res) => {
+    try {
+        const { to, subject, text } = req.body;
+
+        let transporter = nodemailer.createTransport({
+          host: "smtp.office365.com",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: "benj.romeroc@duocuc.cl",
+            pass: "Josebenj@1979"
+          },
+          tls: {
+            rejectUnauthorized: false
+          }
+        });
+
+        const mailOptions = {
+            from: 'benj.romeroc@duocuc.cl',
+            to,
+            subject,
+            text
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Correo enviado', info });
+    } catch (error) {
+        console.error('Error al enviar el correo:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
