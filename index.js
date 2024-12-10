d7b4b53d76fb0f6d36438783745234e7fca01288
benjamin romero <133557177+yolo45osea@users.noreply.github.com>
2024-12-10 00:36:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 587 || 3000;
 
 // Configurar CORS para permitir todo
 app.use(cors());
